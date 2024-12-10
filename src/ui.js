3e5001f5be7c9ff721e0b8a669b0807ec1a4d6d3
Oiyanu <iyanuolatomide@gmail.com>
2024-12-10 20:50:57 +0100

    implementation of renderChat functionality

--- src/ui.js
+++ src/ui.js
@@ -5,4 +5,15 @@ class ChatUI{
     clear(){
         this.list.innerHTML = '';
     }
+    renderChat(data){
+        const when = formatDistanceToNow(data.created_at.toDate(), { addSuffix: true });
+        const html = `<div>
+        <li><span>${data.username}</span>
+        <span>${data.message}</span>
+        <span>${when}</span>
+        </li>
+        </div>`;
+
+        this.list.innerHTML += html;
+    }
 }
